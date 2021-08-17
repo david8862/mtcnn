7741a9a99740bd180deb1596611a68b47f65417b
Xiaobin Zhang <david8862@gmail.com>
2021-08-17 18:58:24 +0800

    add face alignment function

--- example_align.py
+++ example_align.py
@@ -23,15 +23,74 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import numpy as np
 import cv2
+#from skimage import transform as trans
 from mtcnn import MTCNN
+
+# reference from
+# https://github.com/deepinsight/insightface/blob/f61956fda322db9977a9bc250031c079c2eb6192/src/common/face_preprocess.py
+def align_face(image, bbox, keypoints, face_size=(112, 96), show_point=True):
+    # check face size
+    # only support (112,96) and (112,112)
+    assert len(face_size) == 2
+    assert face_size[0] == 112 and (face_size[1] == 112 or face_size[1] == 96)
+
+    # target 5 (x,y) format keypoints in (112,96) shape,
+    # keypoint order: (left_eye, right_eye, nose, mouth_left, mouth_right)
+    target_points = np.array([[30.2946, 51.6963],
+                              [65.5318, 51.5014],
+                              [48.0252, 71.7366],
+                              [33.5493, 92.3655],
+                              [62.7299, 92.2041]], dtype=np.float32)
+    # if use (112, 112) shape, adjust x coordinate
+    if face_size[1] == 112:
+        target_points[:,0] += 8.0
+
+    # src keypoints array
+    src_points = keypoints.astype(np.float32)
+
+    # calculate transform matrix
+    M, _ = cv2.estimateAffine2D(src_points.reshape(1,5,2), target_points.reshape(1,5,2))
+    #tform = trans.SimilarityTransform()
+    #tform.estimate(src_points, target_points)
+    #M = tform.params[0:2,:]
+
+    # do affine transformation to get aligned face image
+    aligned_face = cv2.warpAffine(image, M, (face_size[1], face_size[0]), borderValue=0.0)
+
+    # show keypoints on face image
+    if show_point:
+        for target_point in target_points:
+            cv2.circle(aligned_face, tuple(target_point), 1, (0, 155, 255), 2)
+
+    return aligned_face
+
+
 
 detector = MTCNN()
 
 cv2.namedWindow('MTCNN', cv2.WINDOW_NORMAL)
+cv2.namedWindow('Aligned Face', cv2.WINDOW_NORMAL)
 
 image = cv2.cvtColor(cv2.imread("ivan.jpg"), cv2.COLOR_BGR2RGB)
 results = detector.detect_faces(image)
+
+# only align 1st face
+result = results[0]
+bounding_box = result['box']
+keypoints = result['keypoints']
+
+# bbox in (xmin,ymin,xmax,ymax) format
+bbox = (bounding_box[0], bounding_box[1], bounding_box[0]+bounding_box[2], bounding_box[1]+bounding_box[3])
+# MTCNN 5 keypoint array
+keypoints_array = np.asarray([keypoints['left_eye'], keypoints['right_eye'], keypoints['nose'], keypoints['mouth_left'], keypoints['mouth_right']])
+
+face = align_face(image, bbox=bbox, keypoints=keypoints_array, face_size=(112, 96))
+
+cv2.imshow("Aligned Face", cv2.cvtColor(face, cv2.COLOR_RGB2BGR))
+
+
 
 for result in results:
     # Result is an array with all the bounding boxes detected. We know that for 'ivan.jpg' there is only one.
